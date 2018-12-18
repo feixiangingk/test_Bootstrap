d5b2bec71de5ee56536e9a6b0586e22b130791dd
feixiangingk <feixiangingk@163.com>
2018-12-19 00:04:23 +0800

    2018/12/18 update todo

--- js/todo.js
+++ js/todo.js
@@ -3,10 +3,33 @@ var log = function(){
 }
 
 var el = function(sel){
+
 	return document.querySelector(sel)
 }
 
+//var todoTemplate =function(todo){
+//  var t = '<div class="todo-cell">
+//          <button class="todo-delete">删除</button>
+//          <span>${todo}</span>
+//      </div>'
+//  return t
+//}
+
+var todoTemplate = function(todo){
+    var t = `
+        <div class="todo-cell">
+            <button class="todo-delete">删除</button>
+            <span>${todo}</span>
+        </div>`;
+    return t
+}
+
+
 var b = el('#id-button-add')
+
 b.addEventListener('click',function(){
 	log('yes')
+	var input = el("#id-input-todo")
+	var todo = input.value
+	log("todo: " +todo)
 })
