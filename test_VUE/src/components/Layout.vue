19348065235c2788dd853aea695496088f658294
谷帆 <feixiangingk@163.com>
2018-12-25 19:32:49 +0800

    2018/12/25 谷帆 VUE

---
+++ test_VUE/src/components/Layout.vue
@@ -0,0 +1,68 @@
+<template>
+<el-container class="mainC">
+  <el-header>
+    <swan_header></swan_header>
+  </el-header>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
+
+</template>
+
+<script>
+import swan_header from '@/components/SwanHeader'
+export default {
+  name: 'Layout',
+  components:{
+    swan_header,
+  },
+  data () {
+    return {
+      msg: 'this is layout'
+    }
+  }
+}
+</script>
+
+
+<style>
+  .mainC{
+    height: 900px;
+
+  }
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+</style>
---
+++ test_VUE/src/components/SwanHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <h3>这是header</h3>
+</template>
+<script>
+export default {
+    name:"SwanHeader",
+    data(){
+        return {
+            msg:'this is header'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
